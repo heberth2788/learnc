418eed4f018c7601333b10adb68c40b4074e20ed
Heberth Deza <heberth2788@gmail.com>
2024-11-06 18:01:43 -0500

    Initial Commit

---
+++ hd/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  hd
+//
+//  Created by Heberth Deza on 6/11/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
