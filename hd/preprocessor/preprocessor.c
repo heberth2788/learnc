7f0eb66f5fc55289016d0885e5f7bcb677e5df29
Heberth Deza <heberth2788@gmail.com>
2025-01-27 15:30:55 -0500

    add preprocessor directives and fix .gitignore file

--- hd/preprocessor/preprocessor.c
+++ hd/preprocessor/preprocessor.c
@@ -40,6 +40,7 @@
  To define:
  
     1. Symbolic constants. Util to define named constants.   E.g:     #define PI 3.14159
+                                                 #define START_HD
         * Another alternative is use 'const':                       E.g.     const double PI = 3.14159
  
     2. Macros.
@@ -71,6 +72,131 @@ inline double calcRectArea(double x, double y) {
 #undef PI // Undefine the constant, so it cannot be used below
 
 inline double calcCircleAreaAux(double x) {
-    //return (PI * x * x); // This line issue an error because of PI was #undef previously
+    //return (PI * x * x); // This line issue an error because of PI was #undef above
     return (3.1416 * x * x);
 }
+
+/**
+    #if ... #endif
+    #ifdef  // #if defined(...)
+    #ifndef // #if !defined(...)
+    #elif
+    #else
+    
+    For Conditional Compilation: which preprocesor directives execute and whether parts of your C code compile
+    It evaluates a constant integer expresion.
+ 
+    Conditional preprocessor directives are frequently used to prevent header files from being included multiples
+    times in the same file.
+ 
+    E.g:    #if !defined(MY_CONSTANT)
+            #define MY_CONSTANT 0
+         #endif
+
+*/
+
+void printDebugInfo(void) {
+    #ifdef DEBUG // Compiler just if DEBUG is defined
+        printf("%d", 1988);
+    #endif
+}
+
+/**
+    #error
+    
+    Prints and implementation-dependent message, the tokens are displayed as an error message,
+    and the program does not compile(fatal error).
+ 
+    Format:
+            #error tokens
+ 
+    E.g:    #error 1 - Out of range error // prints 6 tokens as message
+        #error 2 - Error information // prints 5 tokens
+    
+ */
+
+/**
+    #pragma
+ 
+    Causes and implementation-defined action (Beyond  what is conveyed in the language itself)
+ 
+    Format:
+            #pragma tokens
+    E.g:
+        #pragma GCC dependency "parse.y"
+        #pragma GCC dependency "/usr/include/time.h" rerun fixincludes
+ */
+
+/**
+    # and ##
+ 
+    # convert the replacement-text token to a string surrounded by quotes("").
+    
+        E.g:    #define HELLO(x) puts("Hello, " #x);
+ 
+            So execute:     HELLO("Heberth");
+            Result:         puts("Hello, ""Heberth");  // Same as: puts("Hello, Heberth");
+ 
+    ## concat two tokens, it must have two operands.
+    
+        E.g:    #define TOKENCONCAT(x, y) x ## y
+ 
+            So execute: TOKENCONCAT(O, K)
+            Result:     OK
+ */
+
+void hashAndDoubleHash(void) {
+    puts("AB");
+    puts("A" "B");// Same as above
+    
+    #define OK
+    TOKENCONCAT(O, K) // Result: OK , the above symbolic constant
+}
+
+/**
+    #line
+ 
+    Inform to the C compiler of the location in your code where each token came from, this is the
+    file name and the line number.
+ 
+    Format:
+            #line 100
+            #line 100 "file1.c"
+            
+ */
+
+/**
+    Standard C Predefined symbolic constants, these begin and end with two underscores (__)
+    and often are useful for including additional information in error messages.
+ 
+    These indetifiers cannot be used in #define and #undef diretives.
+ 
+        __LINE__
+        __FILE__;
+        __DATE__;
+        __TIME__;
+        __STDC__;
+ */
+
+void showPredefinedSymbolicConstants(void) {
+    __LINE__;
+    __FILE__;
+    __DATE__;
+    __TIME__;
+    __STDC__; // it means: Standard C support(1) or not(0)
+    
+    int length = 100;
+    fprintf(stderr, "Internal error: "
+                    "negative string length "
+                    "%d at %s, line %d.",
+                    length, __FILE__, __LINE__);
+}
+
+void assertTest(void) {
+    int notGreaterThanTen = 1988;
+    // Show error message if false, showing line number a filename
+    assert(notGreaterThanTen <= 10); // if false, call the abort function from <stdlib.h>
+    
+    // if the symbolic constant "#define NDEBUG" is defined, asserts are ignored.
+}
+
--- hd/preprocessor/preprocessor.h
+++ hd/preprocessor/preprocessor.h
@@ -14,27 +14,103 @@
  */
 
 #ifndef preprocessor_h
-#define preprocessor_h
+    #define preprocessor_h
 
-#include <stdio.h>
+    //------ #include
+    #include <stdio.h>
+    #include <assert.h>
 
-#define PI 3.14159
-#define LOG_PI 6.144
+    //------ #define
+    #define PI 3.14159
+    #define LOG_PI 6.144
+    #define AGE 36
 
-#define CIRCLE_AREA(x) ((PI) * (x) * (x))
-#define RECT_AREA(x, y) ((x) * (y))
+    #define CIRCLE_AREA(x) ((PI) * (x) * (x))
+    #define RECT_AREA(x, y) ((x) * (y))
 
-// Use the continuation character '\' to continue in the next line if needed
-#define MY_CALC(x) ((x) * (x) * (x) \
-* (x))
+    // Use the continuation character '\' to continue in the next line if needed
+    #define MY_CALC(x) ((x) * (x) * (x) \
+    * (x))
 
-#undef LOG_PI // Undefine the constant, so it cannot be used below
+    #define getchar() getc(stdin) // Standar library defined as macro
 
-#define getchar() getc(stdin) // Standar library defined as macro
- 
-inline double calcCircleArea(double x);
-inline double calcRectArea(double x, double y);
+    //------ #undef
+    #undef LOG_PI // Undefine the constant, so it cannot be used below
+    // LOG_PI // this will issue an error
 
-inline double calcCircleAreaAux(double x);
+    inline double calcCircleArea(double x);
+    inline double calcRectArea(double x, double y);
+    inline double calcCircleAreaAux(double x);
+
+    //------ #if ... #endif
+    #define MY_CONSTANT 1
+
+    #if !defined(MY_CONSTANT) // if not defined
+        #define MY_CONSTANT 1
+    #endif
+
+    #if defined(MY_CONSTANT) // if defined
+        #define MY_CONSTANT_AUX 2
+    #endif
+
+    //------ #ifndef
+    #ifndef MY_CONSTANT // same to: #if !defined(MY_CONSTANT)
+        #define MY_CONSTANT 1
+    #endif
+
+    //------ #ifdef
+    #ifdef MY_CONSTANT // same to: #if defined(MY_CONSTANT)
+        #define MY_CONSTANT_AUX 2
+    #endif
+
+    //------ #elif ... #else
+    #ifndef MY_CONSTANT // if not defined
+        #define MY_CONSTANT 1
+    #elif MY_CONSTANT == 2 // else if
+        #define MY_CONSTANT_PIVOT 1
+    #elif defined(MY_CONSTANT_PIVOT) // else if
+        #define MY_CONSTANT_PIVOT_AUX 2
+    #else // else
+        #define MY_CONSTANT_PIVOT 3
+    #endif
+
+    // Prevent code from being compiled
+    #if 0 // if false
+        // This block won't be compiled
+    #endif
+
+    // Enabled code to be compiled
+    #if 1 // if true
+        // This block will be compiled
+    #endif
+    
+    void printDebugInfo(void);
+
+    //------ #error
+    #if defined(FOO) && !defined(BAR)
+        // stops the compilation process and show the fatal error
+        #error 1 - Requires FOO and BAR
+        #error "Required FOO and BAR"
+    #endif
+    
+    //------ #pragma
+    #pragma GCC dependency "/usr/lib/dtrace/dt_cpp.h" rerun fixincludes
+    #define strrchr rindex
+    #pragma GCC poison rindex
+
+    //------ #
+    #define HELLO(x) puts("Hello, " #x);
+    
+    //------ ##
+    #define TOKENCONCAT(x, y) x ## y
+
+    void hashAndDoubleHash(void);
+
+    //------ #line
+    #line 100 "fileA.c"
+
+    void showPredefinedSymbolicConstants(void);
+
+    void assertTest(void);
 
 #endif /* preprocessor_h */
